1e71e96dbcab7803aca9549f13cfaec2f9a73ee4
Brian Lawney <brianlawney@hsph.harvard.edu>
2024-06-13 14:43:52 -0400

    updates. runs locally, not tested on mev

--- docker/sthet.R
+++ docker/sthet.R
@@ -1,8 +1,9 @@
 suppressMessages(library(spatialGE))
-suppressMessages(library(reshape2))
+#suppressMessages(library(reshape2))
 suppressMessages(library(rjson))
 suppressMessages(library(optparse))
 suppressMessages(library(dplyr))
+suppressMessages(library(tidyr))
 source('/usr/local/bin/prep_stlist.R')
 
 # args from command line:
@@ -26,12 +27,12 @@ option_list <- list(
         help='Normalization method of `log` or `sct`'
     ),
     make_option(
-        c('-g', '--gene_set'),
-        help='The set of genes to test under SThet.'
+        c('-m', '--method'),
+        help='The spatial statistic to compute. One of `moran` or `geary`'
     ),
     make_option(
-        c('-o', '--output'),
-        help='The name of the output file'
+        c('-g', '--gene_set'),
+        help='The set of genes to test under SThet.'
     )
 )
 
@@ -49,8 +50,7 @@ if (is.null(opt$coordinates_file)){
     quit(status=1)
 }
 
-# ToDo: exception checking for gene set input
-
+genes_to_test <- strsplit(opt$gene_set, ',')[[1]]
 
 # transform the name of the normalization scheme:
 if (is.null(opt$normalization)){
@@ -65,39 +65,64 @@ if (is.null(opt$normalization)){
     quit(status=1)
 }
 
+# transform the name of the test statistic:
+if (is.null(opt$method)){
+    message('Need to provide a test statistic with the -m/--method arg.')
+    quit(status=1)
+} else if(opt$method == "Moran's I"){
+    stat_method <- 'moran'
+} else if(opt$method == "Geary's C"){
+    stat_method <- 'geary'
+} else {
+    message("We only accept `Moran's I` or `Geary's C` for the normalization scheme.")
+    quit(status=1)
+}
+
 # prepare an STList instance. Note that we are potentially re-mapping
 # the original gene identifiers to symbols such that they work with
 # the MSigDB files:
 spat_list <- prep_stlist(opt$input_file, 
                          opt$coordinates_file,
-                         opt$sample_name,
-                         gene_mapping_df,
-                         gene_ids,
-                         'SYMBOL')
+                         opt$sample_name)
 spat <- spat_list$spat
 
 # normalize
 spat <- transform_data(spat, method=opt$normalization)
 
+# check requested genes against this object:
+raw_counts <- spat@counts[[opt$sample_name]]
+diff_set <- setdiff(genes_to_test, rownames(raw_counts))
+num_invalid_genes <- length(diff_set)
+if (num_invalid_genes > 0) {
+    max_print <- 5
+    if (num_invalid_genes < max_print) {
+        invalid_genes <- paste(diff_set[1:num_invalid_genes], collapse=', ')
+    } else {
+        invalid_genes <- sprintf('%s, and %d others.', paste(diff_set[1:max_print], collapse=', '), num_invalid_genes-max_print)
+    }
+    message(sprintf('The following genes to test were not found in your count matrix: %s', invalid_genes))
+    quit(status=1)
+}
+
 # Run SThet
 spat <- SThet(
     spat,
-    genes = opt$gene_set,
-    method = 'moran'
+    genes = genes_to_test,
+    method = stat_method
 )
 
 # Pull dataframe from STList metadata and write to table
-# skips gene column
-# note: there are two column for data in sthet output, moran and geary;
-#   and both are input params to SThet under method
-#   data is placed into appropriate column
-#   will need logic for switching column if we expose moran vs geary method choice
+# skips sample column (the first)
+stat_col <- if(stat_method == 'moran') 'moran_i' else 'geary_c'
+
+out_table <- get_gene_meta(
+    spat, 
+    sthet_only=T
+)[, c('gene', 'gene_mean', 'gene_stdevs', stat_col)] %>% drop_na()
+
 write.table(
-    get_gene_meta(
-        spat, 
-        sthet_only=T
-    )[, c(2,3,4,5)],
-    opt$output,
+    out_table,
+    'sthet_output.tsv',
     sep="\t",
     quote=F,
     row.names=F
